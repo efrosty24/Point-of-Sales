090e42c6f5cae15d1b6a87f437dd81e1041e88d5
AshwinParambil <parambilashwin@gmail.com>
2025-11-22 22:36:01 -0600

    UPDATE
    # Conflicts:
    #	src/backend/routes/admin.orders.routes.js

--- src/backend/routes/admin.orders.routes.js
+++ src/backend/routes/admin.orders.routes.js
@@ -4,10 +4,7 @@ const ctrl = require('../controllers/orders.controller');
 
 
 router.get('/recent', ctrl.recent);
-router.get('/by-product/:productId', ctrl.byProduct);
-router.get('/by-customer/:id', ctrl.byCustomer); 
 router.get('/', ctrl.list);
-router.get('/:id', ctrl.getOne); 
+router.get('/:id', ctrl.getOne);
 router.patch('/:id/reassign-customer', ctrl.reassignCustomer);
-
 module.exports = router;
